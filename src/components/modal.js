808fd11cb0164c66e174e19e0f5a39cd22935ded
Shane C. <shane@scaffoe.com>
2025-11-21 21:09:25 -0500

    Removed everything
    Removed everything to start from scratch because I do not want to
    upgrade the Electron version. I will just import stuff and change what I
    can for now. :/

--- src/components/modal.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Modal(props) {
-  return (
-    <div className="modal-backdrop">
-      <div className="modal">{props.children}</div>
-    </div>
-  )
-}
